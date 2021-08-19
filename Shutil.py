c9ef88a636ec3c252ade697ff25768932117374b
KathieJ <71437063+KathieJ@users.noreply.github.com>
2021-08-19 16:38:07 +0530

    Add files via upload

---
+++ Shutil.py
@@ -0,0 +1,14 @@
+Python 3.9.6 (tags/v3.9.6:db3ff76, Jun 28 2021, 15:26:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import os
+>>> os.getcwd()
+'C:\\Users\\Kathy\\AppData\\Local\\Programs\\Python\\Python39'
+>>> os.mkdir("C:/Users/Kathy/AppData/Local/Programs/Python/Python1234")
+>>> source="C:/Users/Kathy/Desktop/Python project 2/File2.txt"
+>>> destination="
+KeyboardInterrupt
+>>> destination="C:/Users/Kathy/AppData/Local/Programs/Python/Python1234"
+>>> import shutil
+>>> dest=shutil.copy(source,destination)
+>>> dest=shutil.move(source,destination)
+>>> 
